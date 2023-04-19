b655d14d68acc9176a3a9bb787e833e8aad7d5f6
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-04-18 20:26:12 -0500

    Initial commit

---
+++ src/main/java/com/example/lab4_20200638/Lab420200638Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200638;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420200638Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420200638Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20200638/Lab420200638ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200638;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420200638ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
